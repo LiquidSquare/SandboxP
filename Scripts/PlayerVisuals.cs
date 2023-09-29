4b278909b6c24887646474257e1a19395be46328
Neto(Joao Araujo) <bipnerds@gmail.com>
2023-09-29 17:59:27 -0300

    - Player face target.

--- Scripts/PlayerVisuals.cs
+++ Scripts/PlayerVisuals.cs
@@ -21,7 +21,7 @@ public partial class PlayerVisuals : Node3D
 		Player = GetNode<CharacterBody3D>("../");
 		Skeleton = GetNode<Skeleton3D>("%Skeleton3D");
 		AnimationTree = GetNode<AnimationTree>("AnimationTree");
-		TargetNode = GetParent().GetParent().GetNode<Node3D>("%TargetBoxA");
+		TargetNode = GetParent().GetParent().GetNode<Node3D>("%TargetBoxB");
 	}
 
 	public override void _Process(double delta)
@@ -40,9 +40,17 @@ public partial class PlayerVisuals : Node3D
     	
 		AnimationTree.Set("parameters/BlendSpace1D/blend_position", Player.Velocity.Length() / (float)Player.Get("Speed") );
 
-		HorizontalTargetAngle = Mathf.Atan2( TargetNode.GlobalPosition.Z - Player.GlobalPosition.Z, Player.GlobalPosition.X - TargetNode.GlobalPosition.X );
+		HorizontalTargetAngle = Mathf.Atan2( TargetNode.GlobalPosition.Z - Player.GlobalPosition.Z, Player.GlobalPosition.X - TargetNode.GlobalPosition.X ) + Mathf.Pi/2 - Rotation.Y;
+		VerticalTargetAngle = -Mathf.Atan2( TargetNode.GlobalPosition.Z - GlobalPosition.Z, GlobalPosition.Y - TargetNode.GlobalPosition.Y ) - Mathf.Pi/2;
 
-		Skeleton.SetBonePoseRotation(5, new Quaternion(Vector3.Up, HorizontalTargetAngle + Mathf.Pi/2 - Rotation.Y));
+		// Horizontal Only
+		// Skeleton.SetBonePoseRotation(5, new Quaternion(Vector3.Up, HorizontalTargetAngle ));
+
+		// Vertical Only
+		// Skeleton.SetBonePoseRotation(5, new Quaternion(Vector3.Left, VerticalTargetAngle));
+
+		// Vertical and Horizontal
+		Skeleton.SetBonePoseRotation(5, new Quaternion( -VerticalTargetAngle, HorizontalTargetAngle, 0, 1));
 
 	}
 }
