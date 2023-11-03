ce95f29e4363808b080e74e1c23bf30382922a13
Neto(Joao Araujo) <bipnerds@gmail.com>
2023-11-02 21:18:03 -0300

    - Preventing walk stop by changing direction;

--- Scenes/Characters/YBot/States/WalkingState.cs
+++ Scenes/Characters/YBot/States/WalkingState.cs
@@ -9,16 +9,23 @@ public partial class WalkingState : State
 
 	public void UserInput(InputEvent @event)
 	{
-		if (
-			Input.IsActionJustReleased("up")   ||
-			Input.IsActionJustReleased("down") ||
-			Input.IsActionJustReleased("left") ||
-			Input.IsActionJustReleased("right")
-		)
+		if ( releasedMovementInput() && !isHoldingAnyMovementInput() )
 			EmitSignal( SignalName.Transitioned, this , "IdleState" );
 	}
 
 	public void Update(double delta){}
 
 	public void PhysicsUpdate(double delta){}
+
+	private bool releasedMovementInput() => 
+			Input.IsActionJustReleased("up")   ||
+			Input.IsActionJustReleased("down") ||
+			Input.IsActionJustReleased("left") ||
+			Input.IsActionJustReleased("right");
+	
+	private bool isHoldingAnyMovementInput() => 
+			Input.IsActionPressed("up")   ||
+			Input.IsActionPressed("down") ||
+			Input.IsActionPressed("left") ||
+			Input.IsActionPressed("right");
 }
